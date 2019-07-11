097c7c6a22a5b365040ac3adb3ec897f813a0d39
= <gualtiero.frigerio@gmail.com>
2019-07-11 15:43:00 +0200

    added QuestionsListModel

---
+++ WatchQuizApp WatchKit Extension/QuestionsListModel.swift
@@ -0,0 +1,54 @@
+//
+//  QuestionsListModel.swift
+//  WatchQuizApp WatchKit Extension
+//
+//  Created by Gualtiero Frigerio on 11/07/2019.
+//  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+enum AnswerType {
+    case correct
+    case wrong
+    case unanswered
+}
+
+class QuestionsListModel : BindableObject {
+    var didChange = PassthroughSubject<Void, Never>()
+    var questions:[Question]
+    var category:Category
+    private var answers:[Int:AnswerType] = [:]
+    
+    init(category:Category) {
+        self.category = category
+        self.questions = category.questions
+    }
+    
+    func getAnswer(forQuestionId id:Int) -> AnswerType {
+        if let answer = answers[id] {
+            return answer
+        }
+        return .unanswered
+    }
+    
+    func getQuestion(id:Int) -> Question? {
+        for question in questions {
+            if question.id == id {
+                return question
+            }
+        }
+        return nil
+    }
+    
+    func setAnswer(_ answer:String, forQuestionId id:Int) {
+        for index in 0..<questions.count {
+            if questions[index].id == id {
+                let type:AnswerType = questions[index].correctAnswer == answer ? .correct : .wrong
+                answers[id] = type
+            }
+        }
+        didChange.send()
+    }
+}
